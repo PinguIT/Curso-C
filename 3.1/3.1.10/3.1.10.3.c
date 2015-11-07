df9d072e712318d4e77564df9d729c8678cb1c33
PinguIT <chloeboswell1999@gmail.com>
2015-11-07 13:01:22 +0000

    ...

---
+++ 3.1/3.1.10/3.1.10.3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    char letra;
+    
+    printf("Teclea una letra: \n");
+    scanf("%c", &letra);
+    
+    if ((letra=='x') || (letra=='y') || (letra=='z'))  printf("%c es una cifra numerica\n", letra);
+    if ((letra==',') || (letra=='.') || (letra==':') || (letra==';'))  printf("%c es un signo de puntuacion\n", letra);
+    if ((letra=='a') || (letra=='e') || (letra=='i') || (letra=='o') || (letra=='u'))  printf("%c es un vocal\n", letra);
+      else printf("%c es un consonante\n", letra);
+      
+    return 0;
+}    
