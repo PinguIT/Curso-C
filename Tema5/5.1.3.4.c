68941036b1b1756a5088e93029f26cda10ff889b
PinguIT <chloeboswell1999@gmail.com>
2015-12-04 11:09:59 +0000

    ...

--- Tema5/5.1.3.4.c
+++ Tema5/5.1.3.4.c
@@ -15,7 +15,7 @@ int main()
     
     for (i = 0; i < 10; i++)
     {
-     if 
+     
     }
     printf("Highest number: %d",  );
    
---
+++ Tema5/5.2.3.1.c
@@ -0,0 +1,12 @@
+//Un programa que te pida tu nombre y lo muestre en pantalla separando cada letra de la siguiente con un espacio. Por ejemplo, si tu nombre es "Juan", debería aparecer en pantalla "J u a n"
+#include <stdio.h>
+int main()
+{
+    char name [40];
+    
+    printf("Input name: \n");
+    scanf("%s", name);
+    
+    
+    return 0;
+}
