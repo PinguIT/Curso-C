eb194210541c9cde4fcb29418ded17fee80111af
PinguIT <chloeboswell1999@gmail.com>
2015-12-11 18:58:08 +0000

    ...

---
+++ Tema7/7.4.1.c
@@ -0,0 +1,14 @@
+//Crear una función que calcule el cubo de un número real (float). El resultado deberá ser otro número real. Probar esta función para calcular el cubo de 3.2 y el de 5.
+#include <stdio.h>
+
+int main()
+{
+    float number1 = 3.2*3.2*3.2;
+    float number2 = 5*5*5;
+    
+    printf("3.2^3 = %.3f\n", number1);
+    printf("5^3 = %.2f\n", number2);
+    
+    return 0;
+}
+
---
+++ Tema7/7.4.2.c
@@ -0,0 +1,19 @@
+//Crear una función que calcule cual es el menor de dos números enteros. El resultado será otro número entero.
+#include <stdio.h>
+
+int main()
+{
+    int number1, number2, lowest;
+    
+    printf("Input number:\n");
+    scanf("%d", &number1);
+    printf("Input number:\n");
+    scanf("%d", &number2);
+    
+    if (number1<number2)  lowest=number1;
+    if (number2<number1)  lowest=number2;
+    
+    printf("Lowest: %d", lowest);
+    
+    return 0;
+}
