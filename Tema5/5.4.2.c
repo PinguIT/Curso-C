44c25ffa06b7776dce42f12dff41faad854b745b
PinguIT <chloeboswell1999@gmail.com>
2015-12-11 15:31:16 +0000

    ...

--- Tema5/5.4.2.c
+++ Tema5/5.4.2.c
@@ -1,24 +1,31 @@
 //Un programa que prepare espacio para un máximo de 100 nombres (de un máximo de 80 letras cada uno). El usuario deberá ir introduciendo un nombre cada vez,
 //hasta que se pulse Intro sin teclear nada, momento en el que dejarán de pedirse más nombres y se mostrará en pantalla la lista de los nombres
 //que se han introducido hasta entonces.
+
+// Starting from what you have NOW, modify it so that it prints how many strings you typed in.
 #include <stdio.h>
 #include <string.h>
 int main()
 {
     char names[100][80];
-    int i;
+    int i = 0;
+    int j = 0;
     
     do
     {
         printf("Input name:\n");
-        scanf("%s", names[i]);
+        gets(names[i]);
         i++;
-    } while (strcmp(names[i], '\n') != 0); //¿?
+    } 
+    while ((strcmp(names[i-1], "") != 0) && (i<100));
     
-    for (i=0; i<99; i++)
+  //  printf("%d\n", i-1);
+    
+    for (j=0; j<i-1; j++)
     {
-        if (strcmp(names[i], '\n') == 0)   printf("%s  ", names[i]);  //¿?
+      printf("%s  ", names[j]);
     }
+    
     
     return 0;
     
