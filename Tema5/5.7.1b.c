3d0cd72eafbccc655b6078cc48a3a05f08279147
PinguIT <chloeboswell1999@gmail.com>
2015-12-15 17:45:57 +0000

    ...

--- Tema5/5.7.1b.c
+++ Tema5/5.7.1b.c
@@ -1,29 +1,33 @@
-//Un programa que cree un array de 5 números enteros y lo ordene con cada uno de estos tres métodos, mostrando el resultado de los pasos intermedios
 #include <stdio.h>
 int main()
 {
     int a[5];
-    int i, j, aux;
+    int i, j, aux, lower;
     int n = 5;
+    
     //pedimos numeros
     for (i = 0; i < 5; i++)
     {
         printf("Input number: \n");
         scanf("%d", &a[i]);
     }
-    //burbuja
+    
+    //seleccion directa
     for (i = 0; i < n-1; i++)
     {
-        for (j = i+1; j < n; j++)
+        lower = i;
+        for(j = i+1; j < n; j++)
         {
-            if (a[i] > a[j])
+            if (a[j] < a[lower])  lower = j;
+        }
+        if (lower != i)
             {
                 aux = a[i];
-                a[i] = a[j];
-                a[j] = aux;
+                a[i] = a[lower];
+                a[lower] = aux;
             }
-        }
     }
+    
     //demostramos numeros organizados
     for(i = 0; i < 5; i++)
     {
